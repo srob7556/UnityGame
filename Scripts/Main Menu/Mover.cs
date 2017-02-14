d62594aef76290e8f38ef3e16d098d83ad3fd68e
Abdilgani10 <abdilgani@gmail.com>
2017-02-14 21:15:11 +0000

    This function controls the speed of the circles and cubes

---
+++ Scripts/Main Menu/Mover.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+//This scripts controls the speed of circles and cubes.
+public class Mover : MonoBehaviour {
+	float speed;
+	// Use this for initialization
+	void Start () {
+		//it derives the value of speed from the GameController.
+		//GameObject go = GameObject.Find ("GameController");
+		//GameController speedController = go.GetComponent <GameController> ();
+		speed =GameController.speed; //speedController.speed;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//it moves the circles and cubes.
+		if (GameManager.state == GameManager.PLAYING)
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, speed);
+		else
+			GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+			
+	}
+}
