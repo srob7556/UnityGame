1300297ab38c0a2ebb703871af2522c410cdc7b0
Abdilgani10 <abdilgani@gmail.com>
2017-03-29 17:14:05 +0100

    Controls the boundry of the game for destroying the game and ending game

---
+++ Scripts/Main Menu/Boundry.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+//This script controls the boundry for destroying the cubes or ending the the game.
+public class Boundry : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D other) {
+		//On cube entering the boundries Cube prefab will be destroyed.
+		if(other.gameObject.tag == "Obstacle"||other.gameObject.tag == "Gear"||other.gameObject.tag == "Point"||other.gameObject.tag=="Platform"){
+			Destroy(other.gameObject);
+		}
+
+
+
+}
+}
